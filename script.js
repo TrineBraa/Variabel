6ab48b47cb2bbf5b26be59291c20de79337c6360
trinebra <157688079+trinebra@users.noreply.github.com>
2024-02-13 10:21:56 +0100

    changes

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-SelectedColor = '';
+let choosenColor = "";  //Denne variabelen lar meg holde ev farge som gis ved onclick funkjsonen på de forskjellige farge valgene jeg har.
 
 
-function Selected() {
-    document.getElementById("colorChoice").classList.toggle("selectedDiv");
+// denne funksjonen er knyttet til fargevalgene mine. De har en classlist toggle som gjør at de skal kunne toggles av eller på. 
+function Selected(id, color) {
+    // Selected != Selected
+    document.getElementById(id).classList.toggle("selectedDiv");
 
-    console.log()
+    // if (Selected){color}
+    // else {};
+
+    choosenColor = color; // denne variabelen vil også gi oss fargen i knappen vi har valgt. Denne lagres i variabelen vår.
+    console.log(choosenColor);
+
+    // RemoveSelect();
 }
 
+// denne funksjonen gir oss en farge i bakgrunnen når jeg klikker på divene med funksjonnavnet i deres onclick.
+function giveAColor(id) {
+    document.getElementById(id).style.backgroundColor = choosenColor;
+}
+
+//Denne funksjonen vil endre bagrunsfarge på body tagen. denne er knyttet til knappen som har bodyColor i onclick funskjonen.
+function bodyColor() {
+    document.getElementById('body').style.backgroundColor = choosenColor;
+}
+
+//Forsøk på å fjerne fargen etter å ha trykket på en annen div. jeg forsøkte å endre toggle i den første funksjonen til add og denne til remove uten at det gjorde noe da den ble lagt til i den funksjonen.
+function RemoveSelect(id, color) {
+    document.getElementById(id).classList.remove("selectedDiv")
+}
